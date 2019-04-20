d021e48c0dfb676624279cc8b9850aa2ca28b753
toanprox7 <toanpro7x@gmail.com>
2019-04-20 12:28:35 -0700

    add function dictanceKm

--- routers/admin/init.js
+++ routers/admin/init.js
@@ -21,20 +21,20 @@ init = () => {
     cf.wirtelog(err, module.filename)
   });
 
-  // models.sequelize.query(
-  //   `CREATE  FUNCTION IF NOT EXISTS dictanceKm (lat1 FLOAT, lng1 FLOAT, lat2 FLOAT, lng2 FLOAT)
-  //   RETURNS FLOAT
-  //       RETURN 6371 * 2 * ASIN(SQRT(
-  //           POWER(SIN((lat1 - abs(lat2)) * pi()/180 / 2),
-  //           2) + COS(lat1 * pi()/180 ) * COS(abs(lat2) *
-  //           pi()/180) * POWER(SIN((lng1 - lng2) *
-  //           pi()/180 / 2), 2) ));
-  //   `,
-  //   { replacements: [], type: models.sequelize.QueryTypes.CREATE }
-  // ).then(data => {
-  // }).catch((err) => {
-  //   cf.wirtelog(err, module.filename)
-  // });
+  models.sequelize.query(
+    `CREATE  FUNCTION IF NOT EXISTS dictanceKm (lat1 FLOAT, lng1 FLOAT, lat2 FLOAT, lng2 FLOAT)
+    RETURNS FLOAT
+        RETURN 6371 * 2 * ASIN(SQRT(
+            POWER(SIN((lat1 - abs(lat2)) * pi()/180 / 2),
+            2) + COS(lat1 * pi()/180 ) * COS(abs(lat2) *
+            pi()/180) * POWER(SIN((lng1 - lng2) *
+            pi()/180 / 2), 2) ));
+    `,
+    { replacements: [], type: models.sequelize.QueryTypes.CREATE }
+  ).then(data => {
+  }).catch((err) => {
+    cf.wirtelog(err, module.filename)
+  });
   
 }
 module.exports = init;
